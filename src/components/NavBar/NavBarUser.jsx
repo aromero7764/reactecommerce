2a734adb51b3cef6aeff5127eec6cf05ccd847c5
aromero7764 <aromero7764@gmail.com>
2022-10-06 17:51:26 -0400

    ver 0.0.3

--- src/components/NavBar/NavBarUser.jsx
+++ src/components/NavBar/NavBarUser.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 import 'bulma/css/bulma.min.css';
 import PurchasesButton from '../PurchasesButton/PurchasesButton';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCartOpenState } from '../../store/slices/CartOpen.slice';
 import '@creativebulma/bulma-badge/dist/bulma-badge.css'
 import ShoppingCardButton from '../ShoppingCardButton/ShoppingCardButton';
 
 
 const NavBarUser = () => {
     const navigate = useNavigate();
-    const dispatch = useDispatch()
-
-    const cartOpen = useSelector(state => state.cartOpen)
-
-    const setCartIsOpen = () => dispatch(setCartOpenState(!cartOpen))
 
 
     return (
